35ea3ed07ba867abc219d1e135c5c89899c6d075
weffelms <stacie.weffelmeyer@wwt.com>
2018-10-09 21:23:29 -0500

    search form and results tutorial

--- header.php
+++ header.php
@@ -13,6 +13,11 @@
 
     <!--Site header-->
     <header class="site-header">
+
+      <div class="hd-search">
+        <?php get_search_form(); ?>
+      </div>
+
       <h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
       <h5><?php bloginfo('description'); ?>
 
