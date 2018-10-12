d2a0cd1f4ab0b6161ecc2946db87b8f39a4dfd77
weffelms <stacie.weffelmeyer@wwt.com>
2018-10-12 12:32:33 -0500

    widgets tutorial

--- index.php
+++ index.php
@@ -1,18 +1,27 @@
 <?php
 
-get_header();
+get_header(); ?>
 
-if (have_posts()) :
-  while (have_posts()) : the_post();
+<div class="site-content clearfix">
+  <div class="main-column">
+    <?php
+    if (have_posts()) :
+      while (have_posts()) : the_post();
 
-  get_template_part('content', get_post_format());
+      get_template_part('content', get_post_format());
 
-  endwhile;
+      endwhile;
 
-else :
-  echo '<p>No content found</p>';
+    else :
+      echo '<p>No content found</p>';
 
-endif;
+    endif; ?>
+  </div>
+  <?php get_sidebar(); ?>
+</div>
 
+
+
+<?php
 get_footer();
 ?>
